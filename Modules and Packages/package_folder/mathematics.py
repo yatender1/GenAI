1a7cf85963efa3fd20b3c687aeadb5fcad401888
Yatender <69755396+yatender1@users.noreply.github.com>
2026-03-01 19:42:50 +0530

    Add files via upload

---
+++ Modules and Packages/package_folder/mathematics.py
@@ -0,0 +1,19 @@
+def addition(a,b):
+    return a+b
+
+def subtraction(c,d):
+    if c>d:
+        return c-d
+    else:
+        return d-c
+    
+def multiplication(a,b):
+    return(a*b)
+
+def division(a,b):
+    if b==0:
+        return "Enter the valid value"
+    else:
+        return a/b
+
+    
